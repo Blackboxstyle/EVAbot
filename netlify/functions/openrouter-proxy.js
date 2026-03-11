74e09daeae9d78f5fad3e8bd9919c7846da75e48
Blackboxstyle <santiagocorreo911@gmail.com>
2026-03-11 17:39:32 -0400

    cambio de modelo de lenguaje a "mistral"

--- netlify/functions/openrouter-proxy.js
+++ netlify/functions/openrouter-proxy.js
@@ -91,7 +91,7 @@ USO DE LA INFORMACIÓN
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "meta-llama/llama-3.3-70b-instruct:free",
+        model: "mistralai/devstral-small-2505:free",
         messages: [
           { role: "system", content: systemPrompt },
           { role: "user", content: message },
@@ -133,6 +133,7 @@ USO DE LA INFORMACIÓN
     };
   }
 }
+
 
 
 
